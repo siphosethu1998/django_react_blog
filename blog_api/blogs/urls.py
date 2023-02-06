d2821de719d7e8ea1d68de450a18992a388b89c8
siphosethu1998 <siphosethu1998@gmail.com>
2023-02-06 11:24:44 +0200

    added corsheaders dependencies in settings.py

--- blog_api/blogs/urls.py
+++ blog_api/blogs/urls.py
@@ -1,4 +1,9 @@
 from django.urls import path
 
+from . import views
+
 urlpatterns = [
+    path("blogs/", views.blog_list),
+    path("blogs/<int:id>", views.blog_detail),
+    path("blogs/create", views.blog_create),
 ]
--- blog_api/blogs/views.py
+++ blog_api/blogs/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Blog
+from .serializers import BlogSerializer
 
-# Create your views here.
+@api_view(["GET"])
+def blog_list(request):
+    blogs = Blog.objects.all()
+    serializer = BlogSerializer(blogs, many=True)
+
+    return Response(serializer.data)
+
+@api_view(["GET"])
+def blog_detail(request, id):
+    blog = Blog.objects.get(pk=id)
+    serializer = BlogSerializer(blog, many=False)
+
+    return Response(serializer.data)
+
+@api_view(["POST"])
+def blog_create(request):
+    serializer = BlogSerializer(data=request.data)
+
+    if serializer.is_valid():
+        serializer.save()
+
+    return Response(serializer.data)
